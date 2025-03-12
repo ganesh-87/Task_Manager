b2d3a9b43870271fd88d6821f42b9ebae233b043
ganesh-87 <136360520+ganesh-87@users.noreply.github.com>
2025-03-12 21:12:40 +0530

    Update SecurityConfig.java

--- src/main/java/com/taskmanagement/configure/SecurityConfig.java
+++ src/main/java/com/taskmanagement/configure/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));  // Allow frontend origin
+        config.setAllowedOrigins(List.of("https://task-manager-frontend-three-amber.vercel.app"));  // Allow frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);  // Important for handling cookies or authorization headers
